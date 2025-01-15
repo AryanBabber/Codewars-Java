22a420b614919f77262c59f9707ba4b20b0a6570
Aryan Babber <aryanbabber16@gmail.com>
2025-01-15 11:18:22 +0530

    small change, adding angle bracket

--- src/8-kyu/Unfinished Loop - Bug Fixing #1/Kata.java
+++ src/8-kyu/Unfinished Loop - Bug Fixing #1/Kata.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Kata {
     public static List CreateList(int number) {
-        List<Integer> list = new ArrayList();
+        List<Integer> list = new ArrayList<>();
 
         for (int count = 1; count <= number; count++) {
             list.add(count);
